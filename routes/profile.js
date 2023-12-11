470485083200303bfa081eba69c593ae689346fe
Beltzgetari <124810267+Beltzgetari@users.noreply.github.com>
2023-12-11 15:52:10 +0000

    lehenengo ariketak

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const path = require('path')
+const bidea = path.join(__dirname, '../public/uploads')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, bidea)
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix+ '.'+ file.mimetype.split('/')[1])
+  }
+})
+const permit = [
+  'image/png',
+  'image/jpg'
+]
+const upload = multer({ storage: storage ,
+                        limits: {
+                          fileSize: 2 * 1024 * 1024
+                        },
+                        fileFilter: function(req, file, cb){
+                          if (!permit.includes(file.mimetype)){
+                            return cb(new Error('Only .png files allowed my blodel'))
+                          }
+                          cb(null, true)
+                        }})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +35,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', upload.single('avatar'), function (req, res, next) {
     console.log(req.file)
+    console.log(req.body)
     // req.body will hold the text fields, if there were any
     res.send("Jasota")
 })
